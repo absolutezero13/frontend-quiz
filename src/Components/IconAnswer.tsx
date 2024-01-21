b5a891e635f6fe15acb1e8a9814e00ecb7d4972f
ilker <ilker897@gmail.com>
2024-01-21 23:36:52 +0300

    fix: ts issues

--- src/Components/IconAnswer.tsx
+++ src/Components/IconAnswer.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { context } from "../Context/Context";
 import { Typography } from "@mui/material";
 import { CheckCircleOutlined as CheckCircleOutlinedIcon } from "@mui/icons-material";
